026bee6f5c77057ffcec4625666ee41e02090582
k152744 <yasukiyasuki1@icloud.com>
2019-09-18 15:20:16 +0900

    edit_realation

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -1,2 +1,6 @@
 class Comic < ApplicationRecord
+  has_many :scenes
+
+  validates :title, presence: true, uniqueness: true
+  validates :image, presence: true
 end
--- app/models/scene.rb
+++ app/models/scene.rb
@@ -1,2 +1,12 @@
 class Scene < ApplicationRecord
+  belongs_to :user
+  belongs_to :comic
+  has_many :likes
+  has_many :scenes_images
+
+  validates :title, presence: true
+  validates :text, presence: true
+  validates :user_id, presence: true
+  validates :comic_id, presence: true
+  
 end
